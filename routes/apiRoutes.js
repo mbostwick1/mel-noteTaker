b8bb2862c84e73a2a841be032e05f2e026b6e90c
Melanie Bostwick <mbostwick1@gmail.com>
2021-02-03 22:27:03 -0500

    fixed api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,49 @@
-
-//LOAD DATA 
-const dbJson = require("../db/db.json");
+//LOAD DATA
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 //ROUTING
-module.exports = function(app) {
-
+module.exports = function (app) {
   // GET ROUTES
-    app.get("/api/notes", function(req, res) {
-        return res.json(dbJson);
+  app.get("/api/notes", function (req, res) {
+    res.json(data);
+  });
+
+  app.get("/api/notes/:id", function (req, res) {
+    res.json(data[Number(req.params.id)]);
+  });
+
+  // API POST Requests
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let uniqueId = data.length.toString();
+
+    newNote.id = uniqueId;
+    data.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), function (err) {
+      if (err) throw err;
     });
 
-    // API POST Requests
-    app.post("/api/notes", function(req, res) {
-        const newNote = req.body;
-        dbJson.push(newNote);
+    res.json(data);
+  });
 
-        fs.writeFile("../db/db.json", JSON.stringify(dbJson), function (err) {
-            if (err) throw err;
-            console.log("test"); 
-        });
+  // API DELETE NOTES
 
-        return res.json(true);
+  app.delete("/api/notes/:id", function (req, res) {
+    let noteId = req.params.id;
+    let newId = 0;
 
+    data = data.filter((currentNote) => {
+      return currentNote.id != noteId;
     });
-} 
+
+    for (currentNote of data) {
+      currentNote.id = newId.toString();
+      newId++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    res.json(data);
+  });
+};
